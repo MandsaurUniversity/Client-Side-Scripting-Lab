aa1005062b7e1c9d3bb620de2dcda788fe7f273f
Rahul Dhangar <rahuldhangar@gmail.com>
2023-11-30 13:12:11 +0530

    Add addition and subtraction functionality

--- 04-Functions/2-end/unconventional-calculator/assets/scripts/app.js
+++ 04-Functions/2-end/unconventional-calculator/assets/scripts/app.js
@@ -1,4 +1,31 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function add() {
+
+    let enteredNumber = getUserInput();
+    let stringOutput = currentResult + "+" + enteredNumber;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult,  stringOutput)
+}
+
+function subtract() {
+    let enteredNumber = getUserInput();
+    let stringOutput = currentResult + "-" + enteredNumber;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult,  stringOutput)
+}
+
+// add( val1 , 30);
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+
+// outputResult(currentResult,currentText)
 
 // to add a click event: addBtn.addEveb=ntListener('click', add);
 // to get the value from user input: userInput.value
